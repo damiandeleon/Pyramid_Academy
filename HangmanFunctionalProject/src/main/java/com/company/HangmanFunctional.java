bf76d21cc71f05956cf77dc9d0e43decc5867b8d
Damian Deleon <damianmdeleon@gmail.com>
2022-03-11 14:36:39 -0600

    changed for loop to stream on line 81

--- HangmanFunctionalProject/src/main/java/com/company/HangmanFunctional.java
+++ HangmanFunctionalProject/src/main/java/com/company/HangmanFunctional.java
@@ -2,12 +2,12 @@ package com.company;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class HangmanFunctional {
 
@@ -19,6 +19,7 @@ public class HangmanFunctional {
     static File strike5 = new File("/Users/damiandeleonschool/GenSpark/Pyramid_Academy/HangmanFunctionalProject/src/main/resources/Strike5.text");
     static File strike6 = new File("/Users/damiandeleonschool/GenSpark/Pyramid_Academy/HangmanFunctionalProject/src/main/resources/Strike6.text");
     static File strike7 = new File("/Users/damiandeleonschool/GenSpark/Pyramid_Academy/HangmanFunctionalProject/src/main/resources/Strike7.text");
+    static File highScores = new File("/Users/damiandeleonschool/GenSpark/Pyramid_Academy/HangmanFunctionalProject/src/main/resources/HighScores.text");
     static Scanner scanner = new Scanner(System.in);
     static int lives = 7;
     static ArrayList<Character> usedLetters = new ArrayList<Character>();
@@ -26,28 +27,63 @@ public class HangmanFunctional {
     static boolean gameWon = false;
     static int score = 0;
     static String playerName = "";
+    static List<Map<String, Integer>> playerStats = new ArrayList<Map<String, Integer>>();
 
     public static String generateWord() {
         String[] wordList = {"cat", "dog", "horse", "steal", "bite", "hotel", "pocket", "highland", "pig"};
         Random rand = new Random();
         String correctWord = wordList[rand.nextInt(wordList.length)];
+        System.out.println(correctWord);
         return correctWord;
     }
 
+    public static HashMap<String, Integer> addPlayerStats(String name, Integer playerScore) throws IOException {
+        FileWriter myWriter = new FileWriter("/Users/damiandeleonschool/GenSpark/Pyramid_Academy/HangmanFunctionalProject/src/main/resources/HighScores.text");
+        // new stats are the new HashMap pair for the incoming new stats
+        HashMap< String, Integer> newStats = new HashMap< String,Integer> ();
+        newStats.put(name, playerScore);
+
+        //add the new player stats to the playerStats List
+        playerStats.add(newStats);
+        System.out.println(playerStats);
+
+        //Write the newly updated player Stats to the HighScores file
+        myWriter.write(String.valueOf(playerStats));
+        myWriter.close();
+
+
+        return newStats;
+    }
+
+    public static void printOldScores () throws FileNotFoundException {
+        Scanner sc = new Scanner(highScores);
+        sc.useDelimiter("\\Z");
+        String oldText = sc.next();
+        System.out.println("Previous Scores: " + oldText);
+    }
+
     public static void main(String[] args) throws IOException {
-        System.out.println("\n \n \n ");
+        //print out old scores currently in the text file
+        printOldScores();
+        System.out.println("\n ");
+
+        //request player's name and greet them
         System.out.println("****** Welcome to Hangman ******" + "\n" +
                 "What is your name? ");
         playerName = scanner.nextLine();
         System.out.println("============  HI " + playerName.toUpperCase() + "!  ============" + "\n");
 
+        //grab word to start the game by calling the generateWord method
         String correctWord = generateWord();
         char[] letters = new char[correctWord.length()];
-        for (int i = 0; i < letters.length; i++) {
-            letters[i] = '_';
-        }
 
-//        while(gameOver == false){
+// replaced for loop with stream methods to replace each letter with a "_" to hide the letters************
+        char[] finalLetters = letters;
+        IntStream.range(0, letters.length).forEach(i -> {
+            finalLetters[i] = '_';
+        });
+
+
         while(lives > 0){
             if(lives == 7){
                 char[] array = new char[42];
@@ -108,6 +144,7 @@ public class HangmanFunctional {
                 char character = correctWord.charAt(i);
 
 
+
                 if(character == letter){
                     letters[i] = character;
                     correctGuess = true;
@@ -138,8 +175,9 @@ public class HangmanFunctional {
 
 
             if (gameOver){
-                System.out.println("Yes!  The secret word is: " + correctWord + ". You have won!");
+                System.out.println("\uD83D\uDCAF \uD83D\uDCAF \uD83D\uDCAF \uD83D\uDCAF \n Yes!  The secret word is: " + correctWord + ". \n You have won! \n \uD83D\uDCAF \uD83D\uDCAF \uD83D\uDCAF \uD83D\uDCAF");
                 score += 1;
+                System.out.println(playerName + ", your score is: " + score);
                 // here's where you'll write to the file to record scores
 
                 System.out.println("Do you want to play again? (y/n)");
@@ -148,23 +186,22 @@ public class HangmanFunctional {
                 if(playAgain.equals("n")){
                     gameOver  = true;
                     lives = 0;
+                    addPlayerStats(playerName, score);
                     break;
-                } else {
+                } else if(playAgain.equals("y")){
                     gameOver = false;
                     usedLetters.clear();
-                    lives = 4;
-//**************************************************************
-//                    rand = new Random();
-//                    correctWord = wordList[rand.nextInt(10)];
+                    lives = 7;
                     correctWord = generateWord();
                     letters = new char[correctWord.length()];
+
 //       ************* Update with map, filter, or reduce *******************
 
                     for (int i = 0; i < letters.length; i++) {
                         letters[i] = '_';
                     }
                     //****************************************************************
-                }
+                } else {break; }
 
 
             } else if (lives == 0){
@@ -185,7 +222,7 @@ public class HangmanFunctional {
                 } else {
                     gameOver = false;
                     usedLetters.clear();
-                    lives = 4;
+                    lives = 7;
 //**************************************************************
 //                    rand = new Random();
 //                    correctWord = wordList[rand.nextInt(10)];
@@ -203,11 +240,9 @@ public class HangmanFunctional {
             }
 
         }
-
-
-
-
         System.out.println("Exiting Game");
+//        System.out.println("\n" + "Scores" + "" +
+//                "\n" + sc.next());
     }
 
 }
