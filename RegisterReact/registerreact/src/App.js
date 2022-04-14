6ad225f819f2eee1b11bb70b4be8680855aeaee0
Damian Deleon <damianmdeleon@gmail.com>
2022-04-14 08:23:25 -0500

    Updated class to className and other items needed for React

--- RegisterReact/registerreact/src/App.js
+++ RegisterReact/registerreact/src/App.js
@@ -1,9 +1,9 @@
-
 import './App.css';
 
 function App() {
 
-  function showSubmitted() {
+
+  function ShowSubmitted() {
     var firstName = document.getElementById("fName").value;
     var lastName = document.getElementById("lName").value;
     var email = document.getElementById("email").value;
@@ -11,32 +11,50 @@ function App() {
 
     document.getElementById("show").innerHTML =
       firstName + "<br>" + lastName + "<br>" + email + "<br>" + country;
+    // console.log("You clicked me!");
+    
   }
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Student Registration</h1>
-      <div id="design"> 
-        <form  action="/nothing.php">
-            <label for="fName">First Name</label>
-            <input type="text" id="fName" name="firstName" placeholder="Please enter your first name"></input>
+      <div id='design'>
+        <form action='/nothing.php'>
+          <label htmlFor='fName'>First Name</label>
+          <input
+            type='text'
+            id='fName'
+            name='firstName'
+            placeholder='Please enter your first name'
+          ></input>
 
-            <label for="lName">Last Name</label>
-            <input type="text" id="lName" name="lastName" placeholder="Please enter your last name"></input>
+          <label htmlFor='lName'>Last Name</label>
+          <input
+            type='text'
+            id='lName'
+            name='lastName'
+            placeholder='Please enter your last name'
+          ></input>
 
-            <label for="email">Email Address</label>
-            <input type="text" id="email" name="emailAddress" placeholder="Please enter your email address"></input>
+          <label htmlFor='email'>Email Address</label>
+          <input
+            type='text'
+            id='email'
+            name='emailAddress'
+            placeholder='Please enter your email address'
+          ></input>
 
-            <label for="country">Country</label>
-            <select name="country" id="country">
-                <option value="USA">USA</option>
-                <option value="Canada">Canada</option>
-                <option value="Mexico">Mexico</option>
-            </select>
+          <label htmlFor='country'>Country</label>
+          <select name='country' id='country'>
+            <option value='USA'>USA</option>
+            <option value='Canada'>Canada</option>
+            <option value='Mexico'>Mexico</option>
+          </select>
 
-            <input type="button" value="Submit" onclick={showSubmitted}></input>
+          <input type='button' value='Submit' onClick={ShowSubmitted}></input>
         </form>
       </div>
-      <div id="show" class="showtxt"></div>
+      <div id='show' className='showtxt'></div>
     </div>
   );
 }
